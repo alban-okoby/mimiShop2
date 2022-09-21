0bdedf3cd67f297ed5ee2e338c5ca495c82db380
Cerveau++ <77627170+alban-okoby@users.noreply.github.com>
2022-09-21 13:19:25 +0200

    Customize panier page

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ let sideBar = document.querySelector('.side-bar');
 
 document.querySelector('#menu-btn').onclick = () =>{
     sideBar.classList.toggle('active');
-}
+};
 
 document.querySelector('#close-side-bar').onclick = () =>{
     sideBar.classList.remove('active');
